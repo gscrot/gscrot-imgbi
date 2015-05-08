d95c907fdb21b8e9a3846b8517755a2f6c28874f
redpois0n <red@cmail.nu>
2015-05-08 08:02:42 +0200

    run imgbi-client

--- src/gscrot/uploader/imgbi/Imgbi.java
+++ src/gscrot/uploader/imgbi/Imgbi.java
@@ -1,10 +1,49 @@
 package gscrot.uploader.imgbi;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
 
 public class Imgbi {
 	
 	public static String upload(BufferedImage image) throws Exception {
-		return null; // TODO
+		File file = File.createTempFile("imgbiupload", ".png");
+		
+		FileOutputStream fos = new FileOutputStream(file);
+		ImageIO.write(image, "png", fos);
+		fos.close();
+		
+		String response = upload(file);
+		
+		file.delete();
+		
+		return response;
+	}
+	
+	public static String upload(String file) throws Exception {
+		return upload(new File(file));
+	}
+	
+	public static String upload(File file) throws Exception {
+		ProcessBuilder pb = new ProcessBuilder(new String[] { "imgbi-client", "-i", file.getAbsolutePath() });
+		pb.redirectErrorStream(true);
+		Process p = pb.start();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream())); 
+		
+		String response = "";
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			response += line + "\n";
+		}
+		
+		reader.close();
+		
+		return response;
 	}
 }
